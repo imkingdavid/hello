034aa0c360c36c934b34733fe81369e05a2339a9
David King <imkingdavid@gmail.com>
2014-05-11 21:58:15 -0400

    Initial commit

---
+++ ext.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * @package hello
+ * @copyright (c) 2013 David King (imkingdavid)
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+namespace imkingdavid\hello;
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+ * Main extension class for Hello extension.
+ */
+class ext extends \phpbb\extension\base
+{
+}
